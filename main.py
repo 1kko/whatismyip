bbe55175f4a3ca58e3692dbd978e4f207ed5fb44
1kko <me@1kko.com>
2024-09-23 22:51:02 +0900

    better geoip lookup

--- main.py
+++ main.py
@@ -1,18 +1,47 @@
 #!/usr/bin/env python3
 
-from fastapi import FastAPI, Request
-import whois  # whoisdomain for WHOIS lookups
-import asyncio
-from pydantic import BaseModel
+import datetime
+import logging
+import socket
+from logging.handlers import TimedRotatingFileHandler
+
 import uvicorn
+import whois  # whoisdomain for WHOIS lookups
+from apscheduler.schedulers.background import BackgroundScheduler
+from fastapi import FastAPI, Request, HTTPException
+from fastapi.responses import ORJSONResponse
+from geoip2fast import GeoIP2Fast
+from pydantic import BaseModel
 
 app = FastAPI()
 
+# GEOIP2Fast instance for GeoIP lookups
+GEOIP = GeoIP2Fast()
+
+# Configure logging
+log_formatter = logging.Formatter(
+    '%(asctime)s - %(filename)s:%(lineno)d - %(funcName)s - %(message)s')
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+# Console handler
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+console_handler.setFormatter(log_formatter)
+logger.addHandler(console_handler)
+
+# File handler with rotation every 1 days, keeping 7 days of logs
+file_handler = TimedRotatingFileHandler(
+    'service.log', when='D', interval=1, backupCount=7)
+file_handler.setLevel(logging.INFO)
+file_handler.setFormatter(log_formatter)
+logger.addHandler(file_handler)
+
+
 # Define the Pydantic model for the response
-
-
 class WhoisResponse(BaseModel):
     ip_address: str
+    datetime: datetime.datetime
     location: dict
     whois: dict
     headers: dict
@@ -59,14 +88,30 @@ class WhoisResponse(BaseModel):
             }
         }
 
-# Function to fetch location data using aiohttp
+
+# Function to fetch ip location data
+def fetch_ip_location(ip: str):
+    # return whois.whois(ip)
+    return GEOIP.lookup(ip).to_dict()
 
 
-async def fetch_ip_location(ip: str):
-    return whois.whois(ip)
+# GeoIP2Fast database update
+def update_geoip2fast():
+    update_result = GEOIP.update_all(verbose=False)
+    reload_result = GEOIP.reload_data(verbose=False)
+    logging.info(f"{update_result=}")
+    logging.info(f"{reload_result=}")
 
 
-@app.get("/", response_model=WhoisResponse)
+# Create a BackgroundScheduler instance
+scheduler = BackgroundScheduler()
+# Schedule the GeoIP2Fast database update to run every 24 hours
+scheduler.add_job(update_geoip2fast, "interval", days=3)
+# Start the scheduler
+scheduler.start()
+
+
+@app.get("/", response_model=WhoisResponse, response_class=ORJSONResponse)
 async def get_ip_info(request: Request):
     # Extract request headers
     request_headers = dict(request.headers)
@@ -74,9 +119,7 @@ async def get_ip_info(request: Request):
     # Get the client's IP address
     # this is considered runnint in a reverse proxy
     client_ip = request_headers.get("x-real-ip", request.client.host)
-
-    # Create an asyncio task to fetch IP location info
-    ip_location_task = asyncio.create_task(fetch_ip_location(client_ip))
+    logging.info(f"client={client_ip} lookup={client_ip} (self)")
 
     # Perform a WHOIS lookup for the client's IP address or domain
     try:
@@ -85,15 +128,64 @@ async def get_ip_info(request: Request):
         whois_data = {"error": str(e)}
 
     # Await the result of the IP location task
-    ip_data = await ip_location_task
+    ip_data = fetch_ip_location(client_ip)
+    # remove elapsed_time
+    ip_data.pop('elapsed_time', None)
 
     # Return the IP info and WHOIS data as JSON
-    return {
+    response_data = {
         "ip_address": client_ip,
+        "datetime": datetime.datetime.now(tz=datetime.timezone.utc),
         "location": ip_data,
         "whois": whois_data,
         "headers": request_headers,
     }
+    return response_data
+
+
+@app.get("/{domain_ip}", response_model=WhoisResponse, response_class=ORJSONResponse)
+async def get_ip_info(domain_ip: str, request: Request):
+    # Extract request headers
+    request_headers = dict(request.headers)
+    # this is considered runnint in a reverse proxy
+    client_ip = request_headers.get("x-real-ip", request.client.host)
+    logging.info(f"client={client_ip} lookup={domain_ip}")
+
+    # Check if the input is a domain or an IP address
+    if any(c.isalpha() for c in domain_ip):
+        # It's a domain, resolve it to an IP address
+        try:
+            resolved_ip = socket.gethostbyname(domain_ip)
+        except socket.gaierror as e:
+            raise HTTPException(
+                status_code=400, detail=f"Invalid domain: {domain_ip}")
+    else:
+        # It's an IP address
+        resolved_ip = domain_ip
+
+    # Get the client's IP address
+
+    # Perform a WHOIS lookup for the client's IP address or domain
+    try:
+        whois_data = whois.whois(domain_ip)
+    except Exception as e:
+        whois_data = {"error": str(e)}
+
+    # Await the result of the IP location task
+    ip_data = fetch_ip_location(resolved_ip)
+    # remove elapsed_time
+    ip_data.pop('elapsed_time', None)
+
+    # Return the IP info and WHOIS data as JSON
+    response_data = {
+        "ip_address": domain_ip,
+        "datetime": datetime.datetime.now(tz=datetime.timezone.utc),
+        "location": ip_data,
+        "whois": whois_data,
+        "headers": request_headers,
+    }
+    return response_data
 
 if __name__ == "__main__":
+    # start the scheduler
     uvicorn.run(app, host="0.0.0.0", port=8000)
